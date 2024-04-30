275e4ece37c01e8f1ab90cf945d59b6ea88ad544
Daria <daria.shchuplova@mail.ru>
2024-04-30 13:06:17 +0700

    Добавлена передача информации через pipe от процесса-родителя процессу ребенку

--- 05-2.c
+++ 05-2.c
@@ -2,9 +2,11 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
 //Создаем массив из 2ух элементов для хранения файловых дескрипторов для чтения и для записи
 	int fd[2];
+	pid_t result;
 	size_t size;
 	char string[] = "Hello, world!";
 	char resstring[14];
@@ -15,31 +17,47 @@ int main(){
 		//Возврат признака аварийного завершения программ (-1)
 		exit(-1);
 	}
-	//Пробуем записать в pipe 14 байт из нашего массива, т.е. всю строку "Hello, world!" вместе с признаком конца строки
-	size = write(fd[1], string, 14);
-	//Проверака количства байт, записанных в строку с помощью write()
-	if(size != 14){
+	result = fork();
+	if (result<0) {
+		printf("Ошибка выполнения выполнения fork()\n");
+		exit(-1);
+	}
+	else if (result>0) {
+		if (close(fd[0]<0)) {
+			printf("Ошибка при закрытии pipe на чтение\n");
+			exit(-1);
+		}
+		size = write(fd[1], string, 14);
+		//Проверака количства байт, записанных в строку с помощью write()
+		if(size != 14){
 		//Если записалось меньшее количество байт, сообщаем об ошибке
 		printf("Can\'t write all string\n");
 		exit(-1);
+		}	
+		printf("Процесс-родитель записал информацию в pipe и заканчивает работу\n");
+		//Закрываем выходной поток
+		if(close(fd[1]) < 0) {
+			printf("Can\'t close output stream\n");
+		}
 	}
-	//Пробуем прочитать из pip'а 14 байт в другой массив, т.е. всю записанную строку
-	size = read(fd[0], resstring, 14);
-	//Проверка количства байт, прочитанных из pipe() с помощью read()
-	if(size < 0){
-		//Если прочитать не смогли, сообщаем об ошибке
-		printf("Can\'t read string\n");
-		exit(-1);
-	}
-	//Печатаем прочитанную строку
-	printf("%s\n", resstring);
-	//Закрываем входной поток
-	if(close(fd[0]) < 0){
-		printf("Can\'t close input stream\n");
-	}
-	//Закрываем выходной поток
-	if(close(fd[1]) < 0){
-		printf("Can\'t close output stream\n");
+	else {
+		//Закрытие pipe для записи
+		if (close(fd[1]) < 0) {
+			printf("Не получилось закрыть исходящий поток");
+			exit(-1);
+		}
+		size = read(fd[0], resstring, 14);
+		//Проверка количества байт, прочитанных из pipe() с помощью read()
+		if (size != 14) {
+			printf("Не получилось прочитать строку\n");
+			exit(-1);
+		}
+		printf("%s\n", resstring);
+		//Закрытие pipe для чтения
+		if (close(fd[0]) < 0) {
+			printf("Не получилось закрыть входящий поток\n");
+			exit(-1);
+		}
 	}
 	return 0;
 }
