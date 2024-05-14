301bae22458f9c095410ce7619faa1c9b9459b97
Daria <daria.shchuplova@mail.ru>
2024-05-14 12:50:44 +0700

    Добавили комментарии в код программ, работающих с семафорами

--- sems.c
+++ sems.c
@@ -4,21 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-	int semid;
-	char pathname[] = "forftok.ipc";
-	key_t key;
-	struct sembuf mybuf;
+	int semid; //Индентификатор ipc для массивов семафоров
+	char pathname[] = "forftok.ipc"; //Файл, используемый для генерации ipc-ключа
+	key_t key; //шзс- ключ
+	struct sembuf mybuf; //Структура для задания операций над семафором
+	//Генерация  ipc-ключа
 	if((key = ftok(pathname, 0)) < 0) {
 		printf("Не удалось сгенерировать ipc-ключ\n");
 		exit(-1);
 	}
+	//Получение доступа к массиву семафоров по  ipc-ключу
 	if((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
 		printf("Не удалось получить доступ к массиву семафоров\n");
 		exit(-1);
 	}
+	//Заполняем структуру mybuf кодами операций над семафором
 	mybuf.sem_op = -1;
 	mybuf.sem_flg = 0;
 	mybuf.sem_num = 0;
+	//Выполнение операций
 	if(semop(semid, &mybuf, 1) < 0) {
 		printf("Не удалось выполнить операцию над семафором 0\n");
 		exit(-1);
